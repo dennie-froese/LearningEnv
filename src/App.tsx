5679ac989998d7f9e98d135bf8742104fcfc71d1
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-03 18:51:37 +0000

    implemented stateMachine and router logic, added css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { StateMachineProvider } from "./state/StateMachine.jsx";
 import "./App.css";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import Question from "./routes/Question";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to this quiz.</p>
-        <button className="App-button">Proceed to login.</button>
-      </header>
-    </div>
+    <StateMachineProvider>
+      <Router>
+        <Switch>
+          <Route path="/questions">
+            <Question />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </StateMachineProvider>
   );
 }
 
