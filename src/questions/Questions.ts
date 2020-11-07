6f2223bcb7495eaa60722e683336a5c1bd1b490d
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-07 08:51:05 +0000

    added sample question object

---
+++ src/questions/Questions.ts
@@ -0,0 +1,14 @@
+export const questions = {
+  1: {
+    type: "textInput",
+    question: "Please tell us about your day."
+  },
+  2: {
+    type: "range",
+    question: "On a scale from 1 to 10, how was your day."
+  },
+  3: {
+    type: "textInput",
+    question: "Please tell us about your day."
+  }
+};
