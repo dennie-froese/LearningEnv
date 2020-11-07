0e260ad4187614b2557c83ac5de2c1e2f9c2a473
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-07 08:06:02 +0000

    minor style changes to questionTextInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { StateMachineProvider } from "./state/StateMachine.jsx";
 import "./App.css";
-import Home from "./routes/Home";
-import Login from "./routes/Login";
-import Question from "./routes/QuestionRange";
-import QuestionRange from "./routes/QuestionRange";
-import QuestionTextInput from "./routes/QuestionTextInput/index";
+import Router from "./navigation/Router";
 
 function App() {
   return (
     <StateMachineProvider>
-      <Router>
-        <Switch>
-          <Route path="/questionTextInput">
-            <QuestionTextInput />
-          </Route>
-          <Route path="/questionRange">
-            <QuestionRange />
-          </Route>
-          <Route path="/questions">
-            <Question />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <Router />
     </StateMachineProvider>
   );
 }
