7c68f9a20252731f4d28958c8b42d6784ce456ee
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-09 18:23:53 +0000

    added finish route

---
+++ src/routes/Finish/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Finish() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Thank you so much for your time.</p>
+        <p>
+          This is the end of the survey. Your answers have now been submitted.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Finish;
