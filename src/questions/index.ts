3e682b00845c8ef2dd122d377b737ef3e260b95d
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-12 09:53:41 +0000

    changed questions input object to JSON and typed it

--- src/questions/index.ts
+++ src/questions/index.ts
@@ -5,25 +5,35 @@ export interface QuestionInterface {
   additionalInfo: number;
 }
 
-const questions: QuestionInterface[] = [
-  {
+const QuestionIDs = [1, 2, 3, 4];
+
+type QuestionID = typeof QuestionIDs[number];
+
+const Questions: { [key in QuestionID]: QuestionInterface } = {
+  1: {
     id: 1,
     questionType: "textInput",
     questionText: "Please tell us about your day.",
-    additionalInfo: 0
+    additionalInfo: 0,
   },
-  {
+  2: {
     id: 2,
     questionType: "range",
     questionText: "On a scale from 1 to 10, how was your day.",
-    additionalInfo: 10
+    additionalInfo: 10,
   },
-  {
+  3: {
     id: 3,
     questionType: "textInput",
     questionText: "Please tell us about your day.",
-    additionalInfo: 0
-  }
-];
+    additionalInfo: 0,
+  },
+  4: {
+    id: 4,
+    questionType: "textInput",
+    questionText: "Please tell us about you.",
+    additionalInfo: 0,
+  },
+};
 
-export default questions;
+export default Questions;
