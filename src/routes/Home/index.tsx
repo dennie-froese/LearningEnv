39d3e5b5ecf155d647a68f6b8d0d4eb6352ac5cd
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-12 09:54:25 +0000

    WIP firebase init

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const firebaseWrite = () => {
+    // firebase.
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome to this survey!</p>
         <p>We appreciate you taking the time out to answer our questions.</p>
         <p>But first, let's get you logged in:</p>
+        <p></p>
         <Link to="/login">
-          <button className="App-button">Proceed to login!</button>
+          <button className="App-button" onClick={firebaseWrite}>
+            Proceed to login!
+          </button>
         </Link>
       </header>
     </div>
