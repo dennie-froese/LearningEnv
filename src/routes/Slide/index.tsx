a227a5913ba4270c7b83b9a016898500d32bdf9f
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-18 21:32:10 +0000

    cleaned up slide route

--- src/routes/Slide/index.tsx
+++ src/routes/Slide/index.tsx
@@ -4,32 +4,26 @@ import QuestionRange from "../QuestionRange";
 import { SlideInterface } from "../../slides";
 import { Route } from "react-router-dom";
 import SlideIntro from "../SlideIntro";
-import { useSlidesState } from "../../hooks/useSlides";
 
 function Slide(slide: SlideInterface) {
-  const activeSlide = useSlidesState()?.activeSlide;
-  if (slide.id === activeSlide) {
-    return (
-      <Route path="/slides">
-        {slide.slideType === "textInput" ? (
-          <QuestionTextInput
-            question={slide.slideText}
-            questionNumber={slide.id}
-          />
-        ) : slide.slideType === "range" ? (
-          <QuestionRange
-            question={slide.slideText}
-            questionNumber={slide.id}
-            rangeMax={slide.additionalInfo}
-          />
-        ) : (
-          <SlideIntro slideText={slide.slideText} slideNumber={slide.id} />
-        )}
-      </Route>
-    );
-  } else {
-    return <div></div>;
-  }
+  return (
+    <Route path="/slides">
+      {slide.slideType === "textInput" ? (
+        <QuestionTextInput
+          question={slide.slideText}
+          questionNumber={slide.id}
+        />
+      ) : slide.slideType === "range" ? (
+        <QuestionRange
+          question={slide.slideText}
+          questionNumber={slide.id}
+          rangeMax={slide.additionalInfo}
+        />
+      ) : (
+        <SlideIntro slideText={slide.slideText} slideNumber={slide.id} />
+      )}
+    </Route>
+  );
 }
 
 export default Slide;
