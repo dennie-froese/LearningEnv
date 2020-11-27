d2250a38a5bf51c2199794e5fbe196e1d43273c4
Dennie Froese <dennie.froese@santander.co.uk>
2020-11-27 17:09:15 +0000

    finalised questionRange route

--- src/routes/QuestionRange/index.tsx
+++ src/routes/QuestionRange/index.tsx
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 import useTimer from "./../../hooks/useTimer";
 import { useSlidesDispatch } from "../../hooks/useSlides";
+import { Answer } from "../../answers";
 
 interface Props {
   questionNumber: number;
   question: string;
-  rangeMax?: number;
   type: string;
 }
 
-function QuestionRange({
-  questionNumber,
-  question,
-  rangeMax = 6,
-  type,
-}: Props) {
+function QuestionRange({ questionNumber, question, type }: Props) {
   const [value, setValue] = useState<undefined | number>(undefined);
   const dispatch = useSlidesDispatch();
   const { launchTime, restart } = useTimer();
   const checkboxes: JSX.Element[] = [];
-  for (var i = 1; i < rangeMax + 1; i++) {
+  const range = 7;
+  const from =
+    type.includes("1") || type.includes("3") ? "Sehr leicht" : "Sehr niedrig";
+  const to =
+    type.includes("1") || type.includes("4") ? "Sehr schwer" : "Sehr hoch";
+
+  for (var i = 1; i < range + 1; i++) {
     const v = i;
     checkboxes.push(
       <div key={v.toString()}>
@@ -29,11 +30,77 @@ function QuestionRange({
           onClick={() => (value === v ? setValue(undefined) : setValue(v))}
           onChange={() => null}
         />
-        <p>{v}</p>
       </div>
     );
   }
   const finish = () => {
+    const obj =
+      type === "Beispiel_Attribution1"
+        ? { Ausdenken_Attribution1: value?.toString() }
+        : type === "Beispiel_Attribution2"
+        ? { Ausdenken_Attribution2: value?.toString() }
+        : type === "Beispiel_Attribution3"
+        ? { Ausdenken_Attribution3: value?.toString() }
+        : type === "Beispiel_Attribution4"
+        ? { Ausdenken_Attribution4: value?.toString() }
+        : type === "Beispiel_SN1"
+        ? { Ausdenken_SN1: value?.toString() }
+        : type === "Beispiel_SN2"
+        ? { Ausdenken_SN2: value?.toString() }
+        : type === "Beispiel_SN3"
+        ? { Ausdenken_SN3: value?.toString() }
+        : type === "Beispiel_SN4"
+        ? { Ausdenken_SN4: value?.toString() }
+        : type === "Beispiel_Konsens1"
+        ? { Ausdenken_Konsens1: value?.toString() }
+        : type === "Beispiel_Konsens2"
+        ? { Ausdenken_Konsens2: value?.toString() }
+        : type === "Beispiel_Konsens3"
+        ? { Ausdenken_Konsens3: value?.toString() }
+        : type === "Beispiel_Konsens4"
+        ? { Ausdenken_Konsens4: value?.toString() }
+        : type === "Beispiel_Konsistenz1"
+        ? { Ausdenken_Konsistenz1: value?.toString() }
+        : type === "Beispiel_Konsistenz2"
+        ? { Ausdenken_Konsistenz2: value?.toString() }
+        : type === "Beispiel_Konsistenz3"
+        ? { Ausdenken_Konsistenz3: value?.toString() }
+        : type === "Beispiel_Konsistenz4"
+        ? { Ausdenken_Konsistenz4: value?.toString() }
+        : type === "Beispiel_Distinktheit1"
+        ? { Ausdenken_Distinktheit1: value?.toString() }
+        : type === "Beispiel_Distinktheit2"
+        ? { Ausdenken_Distinktheit2: value?.toString() }
+        : type === "Beispiel_Distinktheit3"
+        ? { Ausdenken_Distinktheit3: value?.toString() }
+        : type === "Beispiel_Distinktheit4"
+        ? { Ausdenken_Distinktheit4: value?.toString() }
+        : type === "Beispiel_FA1"
+        ? { Ausdenken_FA1: value?.toString() }
+        : type === "Beispiel_FA2"
+        ? { Ausdenken_FA2: value?.toString() }
+        : type === "Beispiel_FA3"
+        ? { Ausdenken_FA3: value?.toString() }
+        : type === "Beispiel_FA4"
+        ? { Ausdenken_FA4: value?.toString() }
+        : type === "Beispiel_SV1"
+        ? { Ausdenken_SV1: value?.toString() }
+        : type === "Beispiel_SV2"
+        ? { Ausdenken_SV2: value?.toString() }
+        : type === "Beispiel_SV3"
+        ? { Ausdenken_SV3: value?.toString() }
+        : type === "Beispiel_SV4"
+        ? { Ausdenken_SV4: value?.toString() }
+        : type === "Beispiel_GWG1"
+        ? { Ausdenken_GWG1: value?.toString() }
+        : type === "Beispiel_GWG2"
+        ? { Ausdenken_GWG2: value?.toString() }
+        : type === "Beispiel_GWG3"
+        ? { Ausdenken_GWG3: value?.toString() }
+        : type === "Beispiel_GWG4"
+        ? { Ausdenken_GWG4: value?.toString() }
+        : null;
+    setValue(undefined);
     dispatch &&
       launchTime &&
       dispatch({
@@ -42,19 +109,25 @@ function QuestionRange({
           type: type,
           answer: {
             zeit: launchTime - Date.now(),
-            Ausdenken_Attribution1: "1",
+            ...obj,
           },
         },
       });
+    restart();
   };
 
   return (
     <div className="Slide">
       <div className="Slide-container">
         <div className="Slide-header">
-          <p>{`Question: ${questionNumber}`}</p>
           <p>{question}</p>
-          <div className="Slide-range">{checkboxes}</div>
+        </div>
+        <div className="Slide-main">
+          <div className="Slide-range-row-boxes">
+            <p>{from}</p>
+            <div className="Slide-range">{checkboxes}</div>
+            <p>{to}</p>
+          </div>
         </div>
         <div className="Slide-nav">
           <button className="Slide-button" onClick={finish}>
