cdcd9492571e206a6651a1b6d4fa4931680a7f3d
Dennie Froese <dennie.froese@santander.co.uk>
2021-09-11 20:40:09 +0100

    renamed partition in db to be written to with RK for this project

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -24,5 +24,5 @@ const db = firebase.database();
 
 export const saveToDatabase = (string: string, obj: any) => {
   const time = Date.now();
-  return db.ref(`/iteration/${string}/${time}/`).push(obj);
+  return db.ref(`/iterationRK/${string}/${time}/`).push(obj);
 };
