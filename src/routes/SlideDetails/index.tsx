af32e8078a905ed02bf499dc0a9b218bbf16c3b2
Dennie Froese <dennie.froese@santander.co.uk>
2021-11-16 18:46:13 +0000

    trimmed down slideDetails

--- src/routes/SlideDetails/index.tsx
+++ src/routes/SlideDetails/index.tsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { inputValidationOff } from "../../App";
+import React, { useEffect } from "react";
 import { saveToDatabase } from "../../firebase/firebase";
-import { useSlidesDispatch, useSlidesState } from "../../hooks/useSlides";
-import useTimer from "../../hooks/useTimer";
+import { useSlidesState } from "../../hooks/useSlides";
 
 interface Props {
   slideText: string;
@@ -12,54 +10,11 @@ interface Props {
 }
 
 function SlideDetails({ slideText, slideNumber, type, header }: Props) {
-  const [name, setName] = useState<string>("");
-  const [details1, setDetails1] = useState<string>("");
-  const [details2, setDetails2] = useState<string>("");
-  const [details3, setDetails3] = useState<string>("");
-  const [details4, setDetails4] = useState<string>("");
-  const [details5, setDetails5] = useState<string>("");
-  const dispatch = useSlidesDispatch();
   const context = useSlidesState();
-  const { launchTime, restart } = useTimer();
-  const [error, setError] = useState("");
 
   useEffect(() => {
-    context?.user &&
-      dispatch &&
-      saveToDatabase(context?.user, context?.answers);
-  }, [context?.answers, context?.user, dispatch]);
-
-  const finish = () => {
-    if (
-      (name && details1 && details2 && details3 && details4 && details5) ||
-      inputValidationOff
-    ) {
-      dispatch &&
-        launchTime &&
-        dispatch({
-          type: "submit_slide",
-          payload: {
-            type: type,
-            answer: {
-              zeit: launchTime - Date.now(),
-              ...{
-                name: name,
-                details1: details1,
-                details2: details2,
-                details3: details3,
-                details4: details4,
-                details5: details5,
-              },
-            },
-          },
-        });
-      setName("");
-      restart();
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    } else {
-      setError("Bitte überprüfe die Vollständigkeit deiner Angaben.");
-    }
-  };
+    context?.user && saveToDatabase(context?.user, context?.answers);
+  }, [context?.answers, context?.user]);
 
   return (
     <div className="Slide">
